0a447a9a38e562400c3ace360b1ed922e639c9e0
Steve Dondley <s@dondley.com>
2023-01-02 14:45:52 -0500

    add git aliases

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -1,1 +1,17 @@
 alias vim=nvim
+alias vi=nvim
+alias n=nvim
+alias lz="source /root/.zshrc"
+
+# git aliases
+alias gst='git status'
+alias gss='git_short_status'
+alias ga='git_add'
+alias gc='git_commit'
+alias gd='git_diff'
+alias gdh='git_diff_head'
+alias gpa='git_push_all'
+alias ge='git_edit'
+alias glb='git branch --list'
+
+
