79a2eaf66501f4de163c3568afcfe6a4cc301074
Steve Dondley <s@dondley.com>
2023-01-02 10:59:21 -0500

    add tmux nav bindings

--- nvim/init.lua
+++ nvim/init.lua
@@ -90,6 +90,18 @@ if has('persistent_undo')
     " Actually switch on persistent undo
     set undofile
 
+"====[ tmux navigator ]=================
+let g:tmux_navigator_no_mappings = 1
+let g:tmux_navigator_disable_when_zoomed = 1
+nnoremap <silent> <c-h> :up<cr>:TmuxNavigateLeft<cr>
+nnoremap <silent> <c-j> :up<cr>:TmuxNavigateDown<cr>
+nnoremap <silent> <c-k> :up<cr>:TmuxNavigateUp<cr>
+nnoremap <silent> <c-l> :up<cr>:TmuxNavigateRight<cr>
+inoremap <silent> <c-h><c-h> <esc>:w<cr>:TmuxNavigateLeft<cr>
+inoremap <silent> <c-j><c-j> <esc>:w<cr>:TmuxNavigateDown<cr>
+inoremap <silent> <c-k><c-k> <esc>:w<cr>:TmuxNavigateUp<cr>
+inoremap <silent> <c-l><c-l> <esc>:w<cr>:TmuxNavigateRight<cr>
+"nnoremap <silent> {Previous-Mapping} :TmuxNavigatePrevious<cr>
 
 ]]
 
