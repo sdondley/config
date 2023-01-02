8c5db8d2df11dcbe7201ecc18fa7b70564b087d9
Steve Dondley <s@dondley.com>
2023-01-02 03:03:13 -0500

    turn off lazy.nvim notifications

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,8 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup(
 "plugins", { 
+  checker = { enabled = true, notify = false },
+  change_detection = { enabled = true, notify = false },
   ui = {
     icons = {
       cmd = "⌘",
